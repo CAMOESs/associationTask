e69ece3816469731d7b17375ebfd06b2a80edacd
camoes <cam@camoes.fr>
2022-09-18 11:53:55 +0100

    nieme commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
-    has_and_belongs_to_many :foods
+    #has_and_belongs_to_many :foods
+    has_many :order_foods
+    has_many :foods
     belongs_to :customer
     belongs_to :address
 end
