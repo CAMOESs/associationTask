2b0040c423561465373acfc8a7bd5120c21137f1
camoes <cam@camoes.fr>
2022-09-19 23:17:18 +0100

    nieme commit

--- app/models/chinese_food.rb
+++ app/models/chinese_food.rb
@@ -1,6 +1,5 @@
 class ChineseFood < ActiveRecord::Base
-    has_many :order_foods
-    has_many :orders, through: :order_foods
-    has_and_belongs_to_many :shops
+    
+    belongs_to :foods
     
 end
--- app/models/order_food.rb
+++ app/models/order_food.rb
@@ -1,6 +1,5 @@
 class OrderFood < ApplicationRecord
     belongs_to :food
     belongs_to :order
-    belongs_to :city
-    belongs_to :genre
+    
 end
