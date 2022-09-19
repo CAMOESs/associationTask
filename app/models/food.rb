605e44817a5a8de2d9c20d2c43afc50e42f41fed
camoes <cam@camoes.fr>
2022-09-19 23:56:02 +0100

    nieme commit

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,8 +3,8 @@ class Food < ApplicationRecord
     has_many :order_foods
     has_many :orders, through: :order_foods
     belongs_to :shop
-    has_many :ChineseFoods
-    has_many :categories
+    belongs_to :ChineseFood
+    belongs_to :category
     #has_many :categor
 
 end
