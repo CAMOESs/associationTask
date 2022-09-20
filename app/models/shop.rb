78fc70eb0b25cc5893c7455af521a97dd977a2ef
camoes <cam@camoes.fr>
2022-09-20 16:27:31 +0100

    nieme commit

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,7 +2,7 @@ class Shop < ApplicationRecord
     has_many :addresses ,as: :addressable
     has_many :foods
     #has_many :genres
-    #has_one :shop
+    belongs_to :chinesefood
     #has_and_belongs_to_many :genres
     #has_many :categories
 end
